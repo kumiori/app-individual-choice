f70a7d3f02909f6685d3f323cffa7a6b4dd71730
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2023-10-17 18:22:28 +0200

    collapsing hashing

--- choice.py
+++ choice.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import streamlit_survey as ss
 
-
-
 st.set_page_config(
     page_title="Choice choosen",
     page_icon="👋",
+    initial_sidebar_state="collapsed"
 )
 
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 st.header('Choice by choosing')
 st.header("Welcome 👋")
 st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
