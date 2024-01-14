93e90634d3091675e9def0640fbe206ef73c0f3c
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-01-14 21:26:10 +0100

    add production mode (new, singular)

---
+++ pages/test_production.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+if st.secrets["runtime"]["STATUS"] == "Production":
+    st.set_page_config(
+        page_title="Celestial Verse Portal",
+        page_icon="✨",
+        layout="wide",
+        initial_sidebar_state="collapsed"
+    )
+
+    st.markdown(
+        """
+    <style>
+        [data-testid="collapsedControl"] {
+            display: none
+        }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+st.write(st.secrets["runtime"]["STATUS"])
