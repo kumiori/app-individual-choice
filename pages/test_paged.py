2a658b6ce8d01fe5f07b07b28d8efaaa6ad932dd
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-01-28 18:29:38 +0100

    fine tuning

--- pages/test_paged.py
+++ pages/test_paged.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 class PagedContainer:
-    def __init__(self, items, items_per_page=3):
+    def __init__(self, items, items_per_page=3, show_pagination = True):
         self.items = items
         self.items_per_page = items_per_page
         self.current_page = 0
+        self.show_pagination = show_pagination
 
     def display_page(self, page):
         # start_idx = self.current_page * self.items_per_page
@@ -15,7 +16,8 @@ class PagedContainer:
         for item in page_items:
             st.write(item)
         
-        st.write(f"Page {page + 1}/{self.get_total_pages()}")
+        if self.show_pagination:
+            st.write(f"Page {page + 1}/{self.get_total_pages()}")
 
     def get_total_pages(self):
         return (len(self.items) + self.items_per_page - 1) // self.items_per_page
