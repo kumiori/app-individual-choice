6ba06880af138cf674641f271e29fe2b99fab4e3
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-02-06 10:55:24 +0100

    mystery of faith initialising session_state

--- pages/test_workshop.py
+++ pages/test_workshop.py
@@ -38,28 +38,24 @@ st.write(st.secrets["runtime"]["STATUS"])
 # pargager des perspectives
 # pargager des problèmes
 
-
 from streamlit_vertical_slider import vertical_slider 
 from lib.texts import _stream_example, corrupt_string
 from pages.test_geocage import get_coordinates
 from pages.test_injection import CustomStreamlitSurvey
 from streamlit_extras.streaming_write import write as streamwrite 
 import time
-import string
 import streamlit_survey as ss
 from streamlit_extras.row import row
 from streamlit_extras.stateful_button import button as stateful_button
 import random
-from lib.io import create_button, create_dichotomy, create_qualitative, create_yesno, create_next, create_globe, create_textinput, create_checkbox, create_equaliser, fetch_and_display_data, conn
 
-from pages.test_paged import PagedContainer
-# from pages.test_game import display_dictionary_by_indices
-# from pages.test_pleasure import display_dictionary
+from lib.io import fetch_and_display_data, conn
 
-from lib.matrices import generate_random_matrix, encode_matrix, display_matrix
+# from pages.test_paged import PagedContainer
+
 from lib.matrices import generate_random_matrix, encode_matrix, display_matrix
 from lib.dictionary_manip import display_dictionary, display_dictionary_by_indices, display_details_description
-from lib.io import create_button, create_dichotomy, create_qualitative, create_yesno, create_next, create_globe, create_textinput, create_checkbox,create_equaliser
+from lib.io import create_button, create_dichotomy, create_qualitative, create_yesno, create_next, create_globe, create_textinput, create_checkbox, create_equaliser
 
 update_frequency = 500  # in milliseconds
 
@@ -89,18 +85,18 @@ def main():
 
     # survey = ss.StreamlitSurvey("Home")
     col1, col2, col3 = st.columns(3)
-
+    
     if 'page_number' not in st.session_state:
-        st.session_state.page_number = 0
+        st.session_state["page_number"] = 0
     
     if 'damage_parameter' not in st.session_state:
-        st.session_state.damage_parameter = 0.0  # Initial damage parameter
+        st.session_state["damage_parameter"] = 0.0  # Initial damage parameter
     
     if 'location' not in st.session_state:
-        st.session_state.location = None  # Initial damage parameter
+        st.session_state["location"] = None  # Initial damage parameter
     
     if 'coordinates' not in st.session_state:
-        st.session_state.location = None  # Initial damage parameter
+        st.session_state["location"] = None  # Initial damage parameter
 
     # once usage:
     # streamwrite(_stream_once(intro_text, 0))
