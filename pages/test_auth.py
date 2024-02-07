5826bcca8a36f4e141cdf3534574751e7918e9c7
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-02-07 20:53:04 +0100

    authentication on the run

--- pages/test_auth.py
+++ pages/test_auth.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+from streamlit_authenticator import Authenticate
+from datetime import datetime, timedelta
+
 import os
 
 import yaml
@@ -15,7 +18,88 @@ st.write('hashed_passwords', hashed_passwords)
 st.subheader('config')
 st.json(config, expanded=False)
 
-authenticator = stauth.Authenticate(
+class _Authenticate(Authenticate):
+    def login(self, form_name: str, location: str='main') -> tuple:
+        """
+        Creates a login widget.
+
+        Parameters
+        ----------
+        form_name: str
+            The rendered name of the login form.
+        location: str
+            The location of the login form i.e. main or sidebar.
+        Returns
+        -------
+        str
+            Name of the authenticated user.
+        bool
+            The status of authentication, None: no credentials entered, 
+            False: incorrect credentials, True: correct credentials.
+        str
+            Username of the authenticated user.
+        """
+        if location not in ['main', 'sidebar']:
+            raise ValueError("Location must be one of 'main' or 'sidebar'")
+        if not st.session_state['authentication_status']:
+            self._check_cookie()
+            if not st.session_state['authentication_status']:
+                if location == 'main':
+                    login_form = st.form('Connect')
+                elif location == 'sidebar':
+                    login_form = st.sidebar.form('Connect')
+
+                login_form.subheader(form_name)
+                
+                self.username = login_form.text_input('Username').lower()
+                st.session_state['username'] = self.username
+                self.password = login_form.text_input('Password', type='password')
+
+                if login_form.form_submit_button('Login'):
+                    self._check_credentials()
+
+        return st.session_state['name'], st.session_state['authentication_status'], st.session_state['username']
+
+    def _check_credentials(self, inplace: bool=True) -> bool:
+        """
+        Checks the validity of the entered credentials.
+
+        Parameters
+        ----------
+        inplace: bool
+            Inplace setting, True: authentication status will be stored in session state, 
+            False: authentication status will be returned as bool.
+        Returns
+        -------
+        bool
+            Validity of entered credentials.
+        """
+        if self.username in self.credentials['usernames']:
+            try:
+                if self._check_pw():
+                    if inplace:
+                        st.session_state['name'] = self.credentials['usernames'][self.username]['name']
+                        self.exp_date = self._set_exp_date()
+                        self.token = self._token_encode()
+                        self.cookie_manager.set(self.cookie_name, self.token,
+                            expires_at=datetime.now() + timedelta(days=self.cookie_expiry_days))
+                        st.session_state['authentication_status'] = True
+                    else:
+                        return True
+                else:
+                    if inplace:
+                        st.session_state['authentication_status'] = False
+                    else:
+                        return False
+            except Exception as e:
+                print(e)
+        else:
+            if inplace:
+                st.session_state['authentication_status'] = False
+            else:
+                return False
+
+authenticator = _Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
@@ -23,10 +107,10 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
-authenticator.login('Welcome home', 'main')
+authenticator.login('Connect', 'main')
 
 if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main', key='unique_key')
+    authenticator.logout('Disconnect', 'main', key='logout')
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
 elif st.session_state["authentication_status"] is False:
