69f3c245587b50f229232192a2722098f0830d54
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-02-13 08:47:47 +0100

    built components into discourse

--- lib/survey.py
+++ lib/survey.py
@@ -2,11 +2,29 @@ import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_vertical_slider import vertical_slider 
 import streamlit_survey as ss
+import os 
 
-_qualitative_selector = components.declare_component(
-    "qualitative",
-    url='http://localhost:3001'
-)
+if st.secrets["runtime"]["STATUS"] == "Production":
+    st.write(os.path.basename(__file__))
+    root_dir = os.path.dirname(__file__)
+
+    # Print the root directory
+    st.write("Root directory:", root_dir)
+    build_dir = os.path.join(os.path.split(root_dir)[0], "qualitative_selector/frontend/build")
+    st.write("Build directory:", build_dir)
+    _qualitative_selector = components.declare_component("qualitiative", path=build_dir)
+else:
+    _qualitative_selector = components.declare_component(
+        "qualitative",
+        url='http://localhost:3001'
+    )
+    
+
+
+
+
+
+
 
 def _dichotomy(name, question, label, rotationAngle = 0, gradientWidth = 40, invert = False, shift = 0, key=None):
     return _qualitative_selector(component = "dichotomy",
