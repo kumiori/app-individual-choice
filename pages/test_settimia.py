d5ea910ba7f8e0bfb49b534ac65654a4a152424f
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-02-16 08:45:05 +0100

    streamlining apps

--- pages/test_settimia.py
+++ pages/test_settimia.py
@@ -23,13 +23,13 @@ import streamlit.components.v1 as components
 #     )
 
 
-from pages.test_location import conn
+from lib.io import conn
+from streamlit_extras.add_vertical_space import add_vertical_space 
 
-from pages.test_1d import _stream_example, corrupt_string
-from pages.test_geocage import get_coordinates
+from lib.texts import _stream_example, corrupt_string, _stream_once
+from lib.geo import get_coordinates
 from streamlit_extras.streaming_write import write as streamwrite 
 import time
-from pages.test_stream import _stream_once
 import json
 import hashlib
 from lib.io import fetch_and_display_data
@@ -38,6 +38,12 @@ _qualitative_selector = components.declare_component(
     "qualitative",
     url='http://localhost:3000'
 )
+
+from lib.survey import CustomStreamlitSurvey
+from lib.authentication import AuthenticateWithKeys
+import streamlit_survey as ss
+import yaml
+from yaml.loader import SafeLoader
 
 # Initialize read_texts set in session state if not present
 if 'read_texts' not in st.session_state:
@@ -87,6 +93,43 @@ def qualitative_parametric(name, question, areas, key=None):
     data_values  = [1, 2, 10],
     question = question)
 
+with open('data/credentials.yml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+if "access_key" not in st.session_state:
+    print('access_key not in session state')
+    st.session_state['access_key'] = ''
+    print(st.session_state['access_key'])
+    
+authenticator = AuthenticateWithKeys(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+survey = CustomStreamlitSurvey()
+            
+def create_connection(key, kwargs = {}):
+    authenticator = kwargs.get('authenticator')
+    survey = kwargs.get('survey')
+    # _location = survey.data['location']['value']
+    _location = survey.data.get('location', {}).get('value', 'Venegono Superiore, Varese, Italy')
+    st.write(_location)
+    if st.session_state["authentication_status"] is None:
+        try:
+            if authenticator.register_user(' Check • Point ', location = _location,  preauthorization=False):
+                st.success(f'Very good 🎊. We have created a key 🗝️ for you. Keys are a short string of characters, these 🤖 days.\
+                    💨 Here is one for your access ✨ <`{ authenticator.credentials["access_key"] }`> ✨.        \
+                    Keep it in your pocket, add it to your wallet...keep it safe 💭. You will use it to re•open the connection 💫')
+        except Exception as e:
+            st.error(e)
+            
+    else:
+        st.warning("We are already connected. Re•enter using your key, check the connection later.")
+
+
 # 2 bedrooms sabatical house, rent income:
 # interested in the other apt: 7-12d rental = 1500EUR+50
 
@@ -94,6 +137,9 @@ def main():
     # st.title("Welcome to the Singular Mapping. A solid proof? Forget, and Ask the moon: If If-Then is full, is still Luck a useful tool? \n ## We divide by zero. \n ## and it's just fuck*ng pitch black..")
     title = """
     ## This is the Settimia
+    """
+
+    """
         Dynamic information gathering platform
     # Artists/Collaborators/Investors
     
@@ -146,13 +192,13 @@ pdf
     st.markdown(title)
     stream = st.empty()
     
-    # with stream:
-    #     _stream, errors = corrupt_string(title, damage_parameter=0.0)
-    #     st.title(f"System errors number {errors}")
-    #     streamwrite(_stream_once(_stream, damage=0.5), unsafe_allow_html=True)
-    #     with st.spinner('...'):
-    #         time.sleep(3)
-    #     streamwrite(_stream_once("## The Moon will still be able to see you...", damage=0.), unsafe_allow_html=True)
+    with stream:
+        _stream, errors = corrupt_string(title, damage_parameter=0.0)
+        st.title(f"System errors number {errors}")
+        streamwrite(_stream_once(_stream, damage=0.5), unsafe_allow_html=True)
+        # with st.spinner('...'):
+            # time.sleep(3)
+        streamwrite(_stream_once("## ...", damage=0.), unsafe_allow_html=True)
 
     if 'location' not in st.session_state:
         st.session_state.location = None  # Initial damage parameter
@@ -164,14 +210,51 @@ pdf
 
     survey = ss.StreamlitSurvey("The Where?")
 
+
+    access_keys = ["3074dac353f359537cbc2df98821c1ef",
+                   "e447d257840306371622d4b9a8287969",
+                   "7979ea222fa4d8052f2751ac6349f8f3",
+                   "153bb8511021a4c8971614f098983bab",
+                   "705a8a7237084f0d267f07690c614f73",
+                   "e4917fdc9398c37d0f63cf5ac5666f78"]
+    
+    
+    col1, col2, col3 = st.columns([1, 1.2, 1])
+    with col1:
+        st.markdown("## Pick an access key >")
+    
     with col2:
-        st.markdown("## ¿ What is your")
-        location = survey.text_input("location", help="Our location will appear shortly...", value=st.session_state.location)
+        for key in access_keys:
+            st.markdown(f"`{key}`", unsafe_allow_html=True)
+    st.divider()
+   
+    col1, col2, col3 = st.columns([1, 1.2, 1])
+    with col1:
+        st.markdown("## ¿ What is your location")
+    with col2:
+        location = survey.text_input("location", help="Our location will appear shortly...", value=st.session_state.get('location', 'Venegono Superiore, Varese, Italy'))
+
+    # create_connection("connection", kwargs = {"survey": survey, "authenticator": authenticator})
+    authenticator.login('Do you already have a key?')
+    
+    st.divider()
+    
+    if st.session_state["authentication_status"]:
+        st.success('Lovely! 🐉 Fellowship connected!')
+        # st.session_state.current_discourse_page = 9
+        
+        authenticator.logout('Disconnect', 'main', key='disconnect')
+    st.divider()
+    col1, col2, col3 = st.columns([1, 1.2, 1])
+    
+    # with col2:
+        # st.markdown("## ¿ What is your")
+        # location = survey.text_input("_location", help="Our location will appear shortly...", value=st.session_state.location)
     
     if location is not None:
         coordinates = get_coordinates(st.secrets.opencage["OPENCAGE_KEY"], location)
         st.session_state.coordinates = coordinates
-        
+        st.write(coordinates)
         with col1:
             st.markdown("## As a rule of thumb...")
             
@@ -179,12 +262,8 @@ pdf
             st.markdown("## Luck (y)")
             lucky_number = survey.number_input("number", min_value=0, max_value=1000000000)
 
-        # if col3.button("\>"):
-        #     st.info("Next is showing the whole")
-
-
         # Send Data Button
-        if col1.button("Claim your place/Check in"):
+        if col1.button(f"Check in from {location}"):
             data = {
                 "location": survey.data["location"]["value"],
                 "latitude": coordinates[0],
@@ -203,7 +282,7 @@ pdf
             newdata = insert_or_update_data(conn, data)
 
             if newdata:
-                st.success(f"This is your signature \n`` {signature} ``. Keep it in your files, it will allow swift access to the past.")
+                st.success(f"This is your signature \n`` {signature} ``. Keep it in your files, it will allow swift access...")
             else:
                 _default = data["location"]+"-"+str(data["luckynumber"])
                 access = survey.text_input("signature", help="Your fingerprint.")
@@ -212,8 +291,8 @@ pdf
         col1, col3, col3 = st.columns(3) 
                        
         if hasattr(st.session_state, 'coordinates'):
-            if col2.button("Display our Map"):
-                data = fetch_and_display_data(conn, table_name="gathering")
+            if col2.button("Update our Map"):
+                data = fetch_and_display_data(conn, kwargs={"database":"gathering"})
                 
                 stream.markdown("# This is how the moon sees the planet revolving...")
                 # with stream:
