1420da1b0d24d703f4c1bea8ff68349fc7ff265e
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-04-17 10:59:29 +0200

    profiler shaping

--- pages/test_authors-gate.py
+++ pages/test_authors-gate.py
@@ -14,6 +14,31 @@ import streamlit_shadcn_ui as ui
 import pandas as pd
 import datetime
 from streamlit_extras.mandatory_date_range import date_range_picker 
+import re
+
+import streamlit_survey as ss
+
+
+def is_valid_phone(phone):
+    phone_regex = r"^(\+\d{1,3})?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$"
+    phone_regex = r"\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$"
+    phone_regex = r"^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){0,13}\d$"
+
+    phone_regexes = [
+        r"^(\+\d{1,3})?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$",
+        r"\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$",
+        r"^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){0,13}\d$"
+    ]
+
+    for regex in phone_regexes:
+        if re.match(regex, phone):
+            return True
+            
+    # if re.match(phone_regex, phone):
+    #     return True
+
+    return False
+
 
 if st.secrets["runtime"]["STATUS"] == "Production":
     st.set_page_config(
@@ -205,6 +230,7 @@ authenticator = Authenticate(
 
 
 survey = CustomStreamlitSurvey()
+personal_data = ss.StreamlitSurvey(label="personal")
 
 def create_connection(key, kwargs = {}):
     authenticator = kwargs.get('authenticator')
@@ -272,7 +298,7 @@ def main():
         df = pd.DataFrame(data)
         item_count = len(df)
         
-        st.write(df)
+        # st.write(df)
         
         with cols[0]:
             ui.metric_card(title="Total count", content=item_count, description="Keys forged", key="card1")
@@ -302,20 +328,49 @@ def main():
             with st.expander("Personal Details", expanded=False):
                 col1, col2 = st.columns([1, 1])
                 with col1:
-                    name = st.text_input("Name", "")
-                    email = st.text_input("Email", "")
+                    name = personal_data.text_input("Name", key="name")
+                    email = personal_data.text_input("Email", key="email")
                 with col2:
-                    phone = st.text_input("Phone Number", "")
-                    default_start = datetime(2024, 9, 24)
+                    phone = personal_data.text_input("Phone Number (starting with +)", key="phone")
+                    default_start = datetime.datetime(2024, 9, 24)
                     default_end = default_start + timedelta(days=5)
                     # desired_bedrooms = st.number_input("Desired Number of Beds", min_value=1, max_value=2, step=1)
-                    date_range = date_range_picker("Select a date range", default_start=default_start, default_end=default_end)
+                    date_range = date_range_picker("Which days to stay in Athena?", default_start=default_start, default_end=default_end)
                     
-                    
-                additional_comments = st.text_area("Any additional comments or preferences?", "")
+                additional_comments = personal_data.text_area("Any additional comments or preferences?", key="extra")
         
-        
-        
+            if st.button("Review"):
+
+                num_nights = abs((date_range[0] - date_range[1]).days)
+                
+                col1, col2 = st.columns(2)
+                with col2:
+                    if phone:
+                        if is_valid_phone(phone):
+                            st.success("✅ Seems a valid phone number")
+                        else:
+                            st.error("❌ We failed to check, seems an invalid phone number?")
+
+                # Displaying review information in two columns
+                with col1:
+                    st.subheader("Personal Information:")
+                    st.write(f"- Name: {name}\n- Email: {email}\n- Phone Number: {phone if phone else 'not provided'}")
+
+                st.markdown("### All the personal data")
+                st.json(personal_data.data, expanded=False)
+
+            st.write("Next steps")
+            st.write("""
+            - [ ] Review the information
+            - [ ] Click the button below to save your preferences
+            - [ ] Download the booklet
+            - [ ] Disconnect
+            
+            - [ ] Check existence of signature in access keys
+            - [ ] Update data
+            
+            """)
+
         st.divider()
         """_🎊 Booklet download_
         
@@ -324,7 +379,7 @@ def main():
         
         download_pdf()
         
-        authenticator.logout('Commit my changes / Disconnect', 'main', key='disconnect')
+        authenticator.logout('Save my preferences & Disconnect', 'main', key='disconnect')
         # add_vertical_space(13)
         st.divider()
 
