7c3a2daefe8dd6948cddc5041e268e23eaff560a
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-04-17 10:59:40 +0200

    quick test to extend surveys

--- pages/test_survey.py
+++ pages/test_survey.py
@@ -8,3 +8,18 @@ button = survey.button("Submit", key="submit_location")
 
 st.json(survey.data)
 
+
+import streamlit_survey as ss
+
+personal_data = ss.StreamlitSurvey(label="personal")
+col1, col2 = st.columns([1, 1])
+with col1:
+    name = personal_data.text_input("Name", key="name")
+    email = personal_data.text_input("Email", key="email")
+with col2:
+    phone = personal_data.text_input("Phone Number (starting with +)", key="phone")
+    # default_start = datetime.datetime(2024, 9, 24)
+    # default_end = default_start + timedelta(days=5)
+
+
+st.write(personal_data.data)
