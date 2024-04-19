380209ecba5b32378bf92228af12925df22e1bbb
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-04-19 17:13:44 +0200

    analytics

---
+++ pages/test_analytics.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from streamlit_gtag import st_gtag
+
+st.set_page_config(
+    page_title="Google Gtag",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Google Analytics :chart:")
+
+
+st_gtag(
+    key="gtag_send_event_a",
+    id="G-MTGGPQLE4C",
+    event_name="app_main_page",
+    params={
+        "event_category": "test_category_a",
+        "event_label": "test_label_a",
+        "value": 97,
+    },
+)
+
+if st.button("Send Event A"):
+    st_gtag(
+        key="gtag_send_event_b",
+        id="G-MTGGPQLE4C",
+        event_name="send_event_button",
+        params={
+            "event_category": "test_category_b",
+            "event_label": "test_label_b",
+            "value": 97,
+        },
+    )
+    st.toast("Event sent!", icon="👍")
+    
+    # print some emojis with a single character
+    st.write("👍")
