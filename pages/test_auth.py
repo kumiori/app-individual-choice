859ef3f3b8df7340949e42808a3cb8c003a6ec78
kumiori <a.leon.baldelli@gmail.com>
2024-08-03 17:12:11 +0200

    updated streamlit-auth

--- pages/test_auth.py
+++ pages/test_auth.py
@@ -39,65 +39,21 @@ class _Authenticate(Authenticate):
         str
             Username of the authenticated user.
         """
-        if location not in ['main', 'sidebar']:
-            raise ValueError("Location must be one of 'main' or 'sidebar'")
         if not st.session_state['authentication_status']:
-            self._check_cookie()
-            if not st.session_state['authentication_status']:
-                if location == 'main':
-                    login_form = st.form('Connect')
-                elif location == 'sidebar':
-                    login_form = st.sidebar.form('Connect')
+            token = self.cookie_controller.get_cookie()
+            if token:
+                self.authentication_controller.login(token=token)
+            login_form = st.form('Connect')
+            login_form.subheader(form_name)
+            
+            self.username = login_form.text_input('Username').lower()
+            st.session_state['username'] = self.username
+            self.password = login_form.text_input('Password', type='password')
 
-                login_form.subheader(form_name)
-                
-                self.username = login_form.text_input('Username').lower()
-                st.session_state['username'] = self.username
-                self.password = login_form.text_input('Password', type='password')
-
-                if login_form.form_submit_button('Login'):
-                    self._check_credentials()
+            if login_form.form_submit_button('Login'):
+                self._check_credentials()
 
         return st.session_state['name'], st.session_state['authentication_status'], st.session_state['username']
-
-    def _check_credentials(self, inplace: bool=True) -> bool:
-        """
-        Checks the validity of the entered credentials.
-
-        Parameters
-        ----------
-        inplace: bool
-            Inplace setting, True: authentication status will be stored in session state, 
-            False: authentication status will be returned as bool.
-        Returns
-        -------
-        bool
-            Validity of entered credentials.
-        """
-        if self.username in self.credentials['usernames']:
-            try:
-                if self._check_pw():
-                    if inplace:
-                        st.session_state['name'] = self.credentials['usernames'][self.username]['name']
-                        self.exp_date = self._set_exp_date()
-                        self.token = self._token_encode()
-                        self.cookie_manager.set(self.cookie_name, self.token,
-                            expires_at=datetime.now() + timedelta(days=self.cookie_expiry_days))
-                        st.session_state['authentication_status'] = True
-                    else:
-                        return True
-                else:
-                    if inplace:
-                        st.session_state['authentication_status'] = False
-                    else:
-                        return False
-            except Exception as e:
-                print(e)
-        else:
-            if inplace:
-                st.session_state['authentication_status'] = False
-            else:
-                return False
 
 authenticator = _Authenticate(
     config['credentials'],
@@ -118,20 +74,22 @@ elif st.session_state["authentication_status"] is False:
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
 
-
+st.divider()
 try:
-    if authenticator.register_user('Register user', preauthorization=False):
+    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+    if email_of_registered_user:
         st.success('User registered successfully')
 except Exception as e:
     st.error(e)
     
+st.divider()
+    
 try:
-    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password('Forgot password')
-    st.write(username_of_forgotten_password)
+    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
     if username_of_forgotten_password:
         st.success('New password to be sent securely')
-        # Random password should be transferred to user securely
-    else:
+        # The developer should securely transfer the new password to the user.
+    elif username_of_forgotten_password == False:
         st.error('Username not found')
 except Exception as e:
     st.error(e)
