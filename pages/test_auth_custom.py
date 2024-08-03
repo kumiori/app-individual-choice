68869df784fed1e3602bdf9a82b3f5e77691c372
kumiori <a.leon.baldelli@gmail.com>
2024-08-03 18:15:26 +0200

    wrestling models

--- pages/test_auth_custom.py
+++ pages/test_auth_custom.py
@@ -29,8 +29,67 @@ st.json(config)
 # st.json(config)
 
 from typing import Callable, Dict, List, Optional
+from streamlit_authenticator.controllers import AuthenticationController, CookieController
+
+from streamlit_authenticator.models import AuthenticationModel
+from streamlit_authenticator.utilities import Validator
+
+class _AuthenticationModel(AuthenticationModel):
+    def __init__(self, credentials: dict, pre_authorized: Optional[List[str]]=None,
+                 validator: Optional[Validator]=None, auto_hash: bool=True):
+        self.credentials = credentials
+        if 'authentication_status' not in st.session_state:
+            st.session_state['authentication_status'] = None
+        if 'username' not in st.session_state:
+            st.session_state['username'] = None
+        if 'logout' not in st.session_state:
+            st.session_state['logout'] = None
+        if 'name' not in st.session_state:
+            st.session_state['name'] = None
+        self.credentials['usernames'] = {}
+    def login(self, username: str, password: str, max_concurrent_users: Optional[int]=None,
+              max_login_attempts: Optional[int]=None, token: Optional[Dict[str, str]]=None,
+              callback: Optional[Callable]=None) -> bool:
+        st.write('login')
+        
+class _AuthenticationController(AuthenticationController):
+    def __init__(self, credentials: dict, pre_authorized: Optional[List[str]]=None,
+                 validator: Optional[Validator]=None, auto_hash: bool=True):
+
+        """
+        Create a new instance of "AuthenticationController".
+
+        Parameters
+        ----------
+        credentials: dict
+            Dictionary of usernames, names, passwords, emails, and other user data.
+        pre-authorized: list, optional
+            List of emails of unregistered users who are authorized to register.        
+        validator: Validator, optional
+            Validator object that checks the validity of the username, name, and email fields.
+        auto_hash: bool
+            Automatic hashing requirement for the passwords, 
+            True: plain text passwords will be automatically hashed,
+            False: plain text passwords will not be automatically hashed.
+        """
+        self.authentication_model = _AuthenticationModel(credentials,
+                                                        pre_authorized,
+                                                        validator,
+                                                        auto_hash)
+        self.validator = Validator()
 
 class AuthenticateWithKey(Authenticate):
+    def __init__(self, credentials: dict, cookie_name: str, cookie_key: str,
+                 cookie_expiry_days: float=30.0, pre_authorized: Optional[List[str]]=None,
+                 validator: Optional[Validator]=None, auto_hash: bool=True):
+        self.cookie_controller  =   CookieController(cookie_name,
+                                                     cookie_key,
+                                                     cookie_expiry_days)
+        self.authentication_controller  =   _AuthenticationController(credentials,
+                                                                     pre_authorized,
+                                                                     validator,
+                                                                     auto_hash)
+
     def login(self, location: str='main', max_concurrent_users: Optional[int]=None,
               max_login_attempts: Optional[int]=None, fields: Optional[Dict[str, str]]=None,
               captcha: bool=False, clear_on_submit: bool=False, key: str='Login',
@@ -66,12 +125,12 @@ class AuthenticateWithKey(Authenticate):
             login_form = st.form('Connect')
             login_form.subheader(fields['Form name'])
             
-            self.access_key = login_form.text_input('Access key').lower()
-            st.session_state['access_key'] = self.access_key
+            access_key = login_form.text_input('Access key').lower()
+            st.session_state['access_key'] = access_key
 
             if login_form.form_submit_button('Open with key 🔑'):
                 # self._check_credentials()
-                if self.authentication_controller.login(username, password,
+                if self.authentication_controller.login(access_key, '',
                                                         max_concurrent_users,
                                                         max_login_attempts,
                                                         callback=callback, captcha=captcha,
@@ -88,14 +147,12 @@ authenticator = AuthenticateWithKey(
     config['preauthorized']
 )
 
-
-
 if st.session_state['authentication_status']:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
 elif st.session_state['authentication_status'] is False:
-    st.error('Username/password is incorrect')
+    st.error('Access key does not open')
 elif st.session_state['authentication_status'] is None:
     authenticator.login('Connect', 'main')
-    st.warning('Please enter your username and password')
+    st.warning('Please use your access key')
