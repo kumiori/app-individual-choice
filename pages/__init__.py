ef2b0aa8bd0a6532a581d78f2304ae5862cc323d
kumiori <a.leon.baldelli@gmail.com>
2024-09-06 15:03:22 +0200

    page_confing delegated

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 if st.secrets["runtime"]["STATUS"] == "Production":
-    st.set_page_config(
-        page_title="Celestial Verse Portal",
-        page_icon="✨",
-        layout="wide",
-        initial_sidebar_state="collapsed"
-    )
+    # st.set_page_config(
+    #     page_title="Celestial Verse Portal",
+    #     page_icon="✨",
+    #     layout="wide",
+    #     initial_sidebar_state="collapsed"
+    # )
 
     st.markdown(
         """
